a643e104d90a9f00b73a55a350a452b885c8b63c
MonaaEid <monaa_eid@hotmail.com>
2023-06-15 10:55:12 -0700

    update

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'print a statment, THnks MONA EID'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
