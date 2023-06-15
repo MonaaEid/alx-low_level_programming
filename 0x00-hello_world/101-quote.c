1e2c0cff12b3e2da21b46568082243e1244d0073
MonaaEid <monaa_eid@hotmail.com>
2023-06-15 12:30:24 -0700

    update

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+/*
+ * main - Entry point
+ *
+ * Description: 'the program that output string to the stsndard error'
+ *
+ * Return: Always  (Error)
+ */
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+
+}
