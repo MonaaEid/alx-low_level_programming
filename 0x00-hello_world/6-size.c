152b068addbfc80e068b87fe4141cc47123c180f
MonaaEid <monaa_eid@hotmail.com>
2023-06-15 11:34:16 -0700

    update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,8 +10,8 @@ int main(void)
 {
 printf("Size of char: %ld byte(s)\n", sizeof(char));
 printf("Size of int: %ld byte(s)\n", sizeof(int));
-printf("Size of long: %ld byte(s)\n", sizeof(long));
-printf("Size of long long: %ld byte(s)\n", sizeof(long long));
+printf("Size of long int: %ld byte(s)\n", sizeof(long int));
+printf("Size of long long int: %ld byte(s)\n", sizeof(long long int));
 printf("Size of float: %ld byte(s)\n", sizeof(float));
 return (0);
 }
