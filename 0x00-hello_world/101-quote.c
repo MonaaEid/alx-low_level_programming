1a1b57fe9e76d3ba8e4e88c8a84a2511f36b88ee
MonaaEid <monaa_eid@hotmail.com>
2023-06-15 13:55:19 -0700

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,13 @@
 #include <unistd.h>
-/*
+/**
  * main - Entry point
  *
- * Description: 'the program that output string to the stsndard error'
+ * Description: the program that output string to the standard error
  *
- * Return: Always  (Error)
- */
+ * Return: 1 (error)
+ **/
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
-
 }
