8a0bbfce95628da0c95e6942bbe8b7c25c9d42b6
MonaaEid <monaa_eid@hotmail.com>
2023-06-15 10:22:14 -0700

    first commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ /*
+  *A function that print a statment, greetings mona eid
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
