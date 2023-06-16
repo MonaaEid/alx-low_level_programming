919ef4f5ba1ee7db9ebabbcead8aaa89c142d527
MonaaEid <monaa_eid@hotmail.com>
2023-06-16 11:45:54 -0700

    Add digit task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print All single digits
+ *
+ * Return: Always 0 (Success)
+ *
+ **/
+
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n <= 9 ; n++)
+		printf("&d\n", n);
+	return (0);
+}
