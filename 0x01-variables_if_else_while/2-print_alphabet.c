c4c29fa8affd1be39e3ca228597666cd13baf945
MonaaEid <monaa_eid@hotmail.com>
2023-06-16 10:49:57 -0700

    Add third task

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,22 @@
-#
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: the program that output alphabet and print
+ * new line
+ *
+ * Return: Always 0 (Success)
+ **/
+
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		putchar(alpha);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
