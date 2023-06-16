4880288c496bc2223d4ef8aa3bcc47237ee07569
MonaaEid <monaa_eid@hotmail.com>
2023-06-16 09:04:05 -0700

    Add second c file

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Description: the program that print random numbr
+ * and make decision and print (lD) LastDigit
+ *
+ * Return: Always 0 (Success)
+ *
+ **/
+int main(void)
+{
+	int n;
+	int lD;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lD = n % 10;
+	if (n > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, lD);
+	else if (n == 0)
+		printf("Last digit of %d is %d and is 0\n", n, lD);
+	else if (n < 6 && n != 0)
+		printf("Last digit of %d is %d nd is less than 6 and not 0\n", n, lD);
+	return (0);
+}
