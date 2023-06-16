3d0508e1f3c1cd3104d743c4ebbc7eed7ddd4617
MonaaEid <monaa_eid@hotmail.com>
2023-06-16 15:00:11 -0700

    update

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: the program that output all possible
+ * combinations of single-digit numbers and print
+ *
+ *
+ * Return: Always 0 (Success)
+ **/
+
+int main(void)
+{
+	int i = 0;
+	int k;
+
+	while (i < 10)
+	{
+		k = i + 1;
+		while (k < 10)
+		{
+			putchar(i + '0');
+			putchar(',');
+			putchar(' ');
+			putchar(k + '0');
+		k++
+		}
+	i++
+	}
+	return (0);
+}
