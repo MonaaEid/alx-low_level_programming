623b2d9bc5d43cab0a39a121dec72a152c9b04b7
MonaaEid <monaa_eid@hotmail.com>
2023-06-20 06:42:59 -0700

    Add README file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Description: print with Write() function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	write("_putchar\n", 8);
+	return (0);
+
+}
