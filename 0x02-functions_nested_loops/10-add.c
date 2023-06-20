b5907e9733995f04e54d6af3814df222496022fd
MonaaEid <monaa_eid@hotmail.com>
2023-06-20 16:59:24 -0700

    update

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,15 +3,13 @@
  * add - Entry point
  *
  * Description: a function that adds two integers and returns the result.
- * @int: the first number
- * @int: the second  numbers to sum
+ * @a: the first number
+ * @b: the second  numbers to sum
  *
  * Return: Always 0 (Success)
  */
-int add(int, int)
+int add(int a, int b)
 {
-	int a;
-	int b;
 	int sum;
 
 	sum = a + b;
