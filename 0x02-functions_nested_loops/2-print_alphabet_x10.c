dd3dfb8bebc69a14cbc1701f34b05ce8fd028385
MonaaEid <monaa_eid@hotmail.com>
2023-06-20 10:19:05 -0700

    print_alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * print_alphabet - Entry point
+ * print_alphabet_x10 - Entry point
  *
  * Description: a function that prints 10 times
  * the alphabet, in lowercase, followed by a new line
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	char chrs;
 	int i;
