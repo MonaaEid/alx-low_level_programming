75f7fbc6fe2f46538b5f59232cd48c54a4be3dd2
MonaaEid <monaa_eid@hotmail.com>
2023-06-20 10:51:49 -0700

    Add compare letters

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower - Entry point
+ *
+ * Description:  a function that checks for lowercase character.
+ * @c: The character to compare
+ *
+ * Return: Always 0 (Success)
+ */
+int _islower(int c)
+{
+	if (_islower)
+	{
+		return (1);
+	}
+	else
+		return (0);
+
+}
