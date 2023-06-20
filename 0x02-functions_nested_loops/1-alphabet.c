6fbe9e1351e2de07baa4f2540144fbecf4d8b05d
MonaaEid <monaa_eid@hotmail.com>
2023-06-20 09:06:44 -0700

    Add README file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: print the alphabet, in lowercase, followed by
+ * a new line using _putchar()
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char m;
+	for (m = 'a'; m < 'z'; m++ )
+	{
+		_putchar('m');
+	}
+	_putchar('\n');
+	return (0);
+}
