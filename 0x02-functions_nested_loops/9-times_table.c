f3a9d92d67b3d8f8036e6e89f8fa046c1330c0b9
MonaaEid <monaa_eid@hotmail.com>
2023-06-20 15:42:16 -0700

    update

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * times_table - Entry point
+ *
+ * Description: a function that checks for lowercase character.
+ *
+ * Return: Always 0 (Success)
+ */
+void times_table(void)
+{
+	int n, i, sum;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (i = 0; i < 10; i++)
+		{
+		sum = i * n;
+		}
+		return (sum);
+	}
+}
