9ae507762657a61d42772f292758ff47c9650a38
MonaaEid <monaa_eid@hotmail.com>
2023-06-20 18:57:38 -0700

    update

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -29,21 +29,21 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar(multi + '0');
-				}
+					_putchar(multi + '0');	}
 				else if (multi < 100)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(multi / 10 + '0');
-					_putchar(multi % 10 + '0');
-				}
-				_putchar(',');
-				_putchar(' ');
-				_putchar((multi / 100) + '0');
-				_putchar((multi / 10) % 10 + '0');
-				_putchar((multi % 10) + '0');
+					_putchar(multi % 10 + '0');	}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((multi / 100) + '0');
+					_putchar((multi / 10) % 10 + '0');
+					_putchar((multi % 10) + '0');	}
 			}
 			_putchar('\n');
 		}
