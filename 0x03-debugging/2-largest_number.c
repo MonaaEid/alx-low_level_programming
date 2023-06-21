a51bf325c3b68125d748219f2d895ac953a40554
MonaaEid <monaa_eid@hotmail.com>
2023-06-21 11:00:13 -0700

    update

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,17 +14,15 @@ int largest_number(int a, int b, int c)
 
 	if (a > b && a > c)
 	{
-		return (largest);
 		largest = a;
 	}
 	else if (b > a && b > c)
 	{
 		largest = b;
-		return (largest);
 	}
 	else
 	{
 		largest = c;
-		return (largest);
 	}
+		return (largest);
 }
