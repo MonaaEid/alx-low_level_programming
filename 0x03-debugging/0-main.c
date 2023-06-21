f5a5470aa5f92f26b22b6f0e00e342344a8193f0
MonaaEid <monaa_eid@hotmail.com>
2023-06-21 12:21:47 -0700

    update

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,16 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * main - tests function that prints if integer is positive or negative
- *
- *
- * Return: Always 0 (Success)
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 
 int main(void)
 {
 	int i;
 
 	i = 98;
+
 	positive_or_negative(i);
 	return (0);
 }
