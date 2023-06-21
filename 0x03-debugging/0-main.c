3fed9d695947d49a680cb92ad9e4baf029a6353f
MonaaEid <monaa_eid@hotmail.com>
2023-06-21 12:16:01 -0700

    update

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,8 +3,6 @@
 /**
  * main - tests function that prints if integer is positive or negative
  *
- * Description: This file must test that the function positive_or_negative()
- * gives the correct output when given a case of 0.
  *
  * Return: Always 0 (Success)
  */
