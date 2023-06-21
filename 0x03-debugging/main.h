fbf6d0282422de22c9214bd2d341349901e4bfd6
MonaaEid <59332150+MonaaEid@users.noreply.github.com>
2023-06-21 21:14:24 +0300

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,9 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdio.h>
-
-int positive_or_negative(int i);
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @chr: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
