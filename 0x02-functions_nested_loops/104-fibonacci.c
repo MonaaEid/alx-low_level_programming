da24e06b097ad7761b3855345c031dac94ee355a
MonaaEid <monaa_eid@hotmail.com>
2023-06-20 19:42:54 -0700

    update

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,1 +1,23 @@
-:
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	unsigned long int a = 1, b = 2, c;
+
+	printf("%lu, %lu", a, b);
+	for (i = 2; i < 98; i++)
+	{
+		c = a + b;
+		printf(", %lu", c);
+		a = b;
+		b = c;
+	}
+	printf("\n");
+	return (0);
+}
