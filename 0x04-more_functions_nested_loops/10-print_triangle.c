e6c6c3e96c3e90e99d974b2bd96e54f6d7589f35
MonaaEid <monaa_eid@hotmail.com>
2023-06-22 16:23:33 -0700

    update

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -2,10 +2,30 @@
 /**
  * print_triangle -a function that prints a square, followed by a new line.
  * @size: is the size of the triangle
- * Return: Use the character # to print the square
+ * Return: Use the character # to print the triangle
  */
 
 void print_triangle(int size)
-{:wq
+{
+	int i, triangle, space;
 
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (int i = 1; i <= size; i++)
+		{
+			for (space = 1; space <= size - i; space++)
+			{
+				_putchar(' ');
+			}
+			for (triangle = 1; triangle <= i; triangle++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
 }
