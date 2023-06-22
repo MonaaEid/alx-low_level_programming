5bfb140fc1ac7ba820bf858d61fb650f82230e59
MonaaEid <monaa_eid@hotmail.com>
2023-06-22 14:21:59 -0700

    update

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
+ *
  * Description: Fizz-Buzz function
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int nums;
 
-	for (nums = 1; nums <= 100 ; nums++)
+	for (nums = 1; nums <= 100; nums++)
 	{
 		if ((nums % 15) == 0)
 		{
-                        printf("FizzBuzz ");
+			printf("FizzBuzz ");
 		}
 		else if ((nums % 3) == 0)
 		{
