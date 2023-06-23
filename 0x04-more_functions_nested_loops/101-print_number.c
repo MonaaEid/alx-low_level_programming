3483b38ce0e8dffcfc4375e292d68a67ebf097cc
MonaaEid <monaa_eid@hotmail.com>
2023-06-22 17:53:44 -0700

    test

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_number - Write a function that prints an integer.
+ * @n: is the size of the triangle
+ * Return: Use the character # to print the triangle
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10)
+	{
+	_putcharn(n / 10 + '0');
+	}
+	_putchar(n % 10 + '0');
+}
