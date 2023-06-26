e02934ec7ba9c17f1ad96eb266cce9c77628fec8
MonaaEid <monaa_eid@hotmail.com>
2023-06-26 01:39:11 -0700

    task No. five

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,11 +7,15 @@
  */
 void print_rev(char *s)
 {
-	int i;
+	int i = 0;
 
-	while (s[i] == '\0')
+	while (s[i] != '\0')
 	{
-		_putchar(s[i]);
+		i++;
+	}
+	while (i >= 0)
+	{
+		_putchar([i]);
 		i--;
 	}
 	_putchar('\n');
