39f60ac13feccd192b2c2eb7b08cb43529bee7a7
MonaaEid <monaa_eid@hotmail.com>
2023-06-26 15:29:13 -0700

    test

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -13,24 +13,24 @@ int _atoi(char *s)
 	int base = 0;
 	int i = 0;
 
-	while (str[i] == ' ')
+	while (s[i] == ' ')
 	{
 		i++;
 	}
-	if (str[i] == '-' || str[i] == '+')
+	if (s[i] == '-' || s[i] == '+')
 	{
-		sign = 1 - 2 * (str[i++] == '-');
+		sign = 1 - 2 * (s[i++] == '-');
 	}
-	while (str[i] >= '0' && str[i] <= '9')
+	while (s[i] >= '0' && s[i] <= '9')
 	{
-	if (base > INT_MAX / 10 || (base == INT_MAX / 10 && str[i] - '0' > 7))
+	if (base > INT_MAX / 10 || (base == INT_MAX / 10 && s[i] - '0' > 7))
 	{
 		if (sign == 1)
 			return (INT_MAX);
 		else
 			return (INT_MIN);
 	}
-	base = 10 * base + (str[i++] - '0');
+	base = 10 * base + (s[i++] - '0');
 	}
 	return (base * sign);
 }
