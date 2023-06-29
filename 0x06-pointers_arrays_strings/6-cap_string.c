a834facfc6d522a2d8dbe16aded319594b4db56e
MonaaEid <monaa_eid@hotmail.com>
2023-06-29 16:01:11 -0700

    test

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,15 +8,29 @@
 char *cap_string(char *str)
 {
 	int i;
+	int j;
+	char c[] = {44, 59, 46, 33, 63, 34, 40, 41, 123, 125, 32, 10, 9};
 
-	for (i = 0; str[i] != '\0'; i++)
+	i = 0;
+	while (str[i] != '\0')
 	{
-		if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' ||
-				str[i - 1] == '\n')
+		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
 		{
-			str[i] = toupper(str[i]);
+			str[i] = str[i] - 32;
 		}
+		j = 0;
+		while (c[j] != '\0')
+		{
+			if (str[i] == c[j])
+			{
+				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				{
+					str[i + 1] = str[i + 1] - 32;
+				}
+			}
+			j++;
+		}
+		i++;
 	}
 	return (str);
-
 }
