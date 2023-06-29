de571e66de5eeb55443c87b162b6e3c77347b8a2
MonaaEid <monaa_eid@hotmail.com>
2023-06-29 12:42:29 -0700

    update

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,39 +1,36 @@
 #include <stdio.h>
 #include <ctype.h>
+#include "main.h"
+/**
+ * print_buffer - a function that prints a buffer.
+ * @b: bytes of the buffer
+ * @size: size of bytes
+ */
 
 void print_buffer(char *b, int size)
 {
-        int i;
-        int j;
+	int i;
+	int j;
 
-        if (size <= 0)
-        {
-                printf("\n");
-                return;
-        }
-        for (i = 0; i < size; i += 10)
-        {
-                printf("%08x: ", i);
-                for (j = 0; j < 10; j++)
-                {
-                        if (i + j < size)
-                                printf("%02x", b[i + j]);
-                        else
-                                printf("  ");
-                        if (j % 2 == 1)
-                                printf(" ");
-                }
-                for (j = 0; j < 10 && i + j < size; j++)
-                        printf("%c", isprint(b[i + j]) ? b[i + j] : '.');
-                printf("\n");
-        }
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
+	for (i = 0; i < size; i += 10)
+	{
+		printf("%08x: ", i);
+		for (j = 0; j < 10; j++)
+		{
+			if (i + j < size)
+				printf("%02x", b[i + j]);
+			else
+				printf("  ");
+			if (j % 2 == 1)
+				printf(" ");
+		}
+		for (j = 0; j < 10 && i + j < size; j++)
+			printf("%c", isprint(b[i + j]) ? b[i + j] : '.');
+		printf("\n");
+	}
 }
-
-int main(void)
-{
-        char buffer[] = "Hello, world!";
-
-        print_buffer(buffer, sizeof(buffer));
-        return (0);
-}
-
