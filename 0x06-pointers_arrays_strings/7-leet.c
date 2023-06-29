fd0748a7b030d870308f61d7bed1fb8594b3ac34
MonaaEid <monaa_eid@hotmail.com>
2023-06-29 16:05:22 -0700

    test

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,13 +9,14 @@ char *leet(char *str)
 	char *p = str;
 	char *table[] = {"4", "3", "0", "7", "1"};
 	char *letters[] = {"aA", "eE", "oO", "tT", "lL"};
+	int i, j;
 
-	for (int i = 0; i < 5; i++)
+	for (i = 0; i < 5; i++)
 	{
 		char *letter = letters[i];
 		char *encoded = table[i];
 
-		for (int j = 0; p[j] != '\0'; j++)
+		for (j = 0; p[j] != '\0'; j++)
 		{
 			if (p[j] == letter[0] || p[j] == letter[1])
 			{
