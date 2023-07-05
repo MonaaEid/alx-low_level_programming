d496608417102aeec59ec4f8f791819698ad0af9
MonaaEid <monaa_eid@hotmail.com>
2023-07-05 15:36:08 -0700

    update

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,30 +3,15 @@
  * natural square root of a number.
  * @n: number
  * @i: current guess
- * @k: upper bound
  * Return: natural square root of n
  */
-int _sqrt_examine(int n, int i, int k)
+int _sqrt_examine(int n, int i)
 {
-	int mid;
-
-	if (k >= i)
-	{
-		mid = (i + k) / 2;
-		if (mid * mid == n)
-		{
-			return (mid);
-		}
-		else if (mid * mid > n)
-		{
-			return (_sqrt_examine(n, i, mid - 1));
-		}
-		else
-		{
-			return (_sqrt_examine(n, mid + 1, k));
-		}
-	}
-	return (-1);
+	if (i * i == n)
+		return (i);
+	if (i * i > n)
+		return (-1);
+	return (_sqrt_examine(n, i + 1));
 }
 /**
  * _sqrt_recursion - a function that returns the natural
@@ -38,10 +23,10 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	else if (n == 0 || n == 1)
+	if (n == 0 || n == 1)
 	{
 		return (n);
 	}
-	return (_sqrt_examine(n, 1, n));
+	return (_sqrt_examine(n, 1));
 }
 
