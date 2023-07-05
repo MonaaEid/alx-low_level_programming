063f8382e6ce722fe6d7dbcf4eea7a056d48c6da
MonaaEid <monaa_eid@hotmail.com>
2023-07-05 15:00:14 -0700

    update

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,17 @@
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to check
+ *
+ * Return: 1 if s is a palindrome, 0 otherwise
+ */
+int is_palindrome(char *s)
+{
+	int len = strlen(s);
+
+	if (len <= 1)
+		return (1);
+	if (s[0] != s[len - 1])
+		return (0);
+	s[len - 1] = '\0';
+	return (is_palindrome(s + 1));
+}
