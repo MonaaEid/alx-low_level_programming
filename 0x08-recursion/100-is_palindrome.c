c68357e922d40e6b775dc91250731685eab710fb
MonaaEid <monaa_eid@hotmail.com>
2023-07-05 18:19:13 -0700

    update

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,41 +2,35 @@
 #include <string.h>
 #include <stdio.h>
 /**
+ * isPalRec - checks if a string is a palindrome
+ * @s: string to check
+ * @str: string
+ * @e: int
+ * Return: 1 if s is a palindrome, 0 otherwise
+ */
+
+int isPalRec(char str[], int s, int e)
+{
+	if (s == e)
+		return (1);
+
+	else if (str[s] != str[e])
+		return (1);
+	else
+		return (isPalRec(str, s + 1, e - 1));
+}
+
+/**
  * is_palindrome - checks if a string is a palindrome
  * @s: string to check
  *
  * Return: 1 if s is a palindrome, 0 otherwise
  */
-/*int is_palindrome(char *s)
- *{
- *
- * 	int len = strlen(s);
- *
- *   if (len <= 1)
- *       return (1);
- *   if (*s != *(s + len - 1))
- *       return (0);
- *   *(s + len - 1) = '\0';
- *   return (is_palindrome(s + 1));
- * }
- */
-int isPalRec(char str[],int s, int e)
-{
-	if (s == e)
-		return (1);
-	
-	else if (str[s] != str[e])
-		return (0);
-	else
-		return (isPalRec(str, s + 1, e - 1));
-}
-
 int is_palindrome(char *s)
 {
-int n = strlen(s);
+	int n = strlen(s);
 
-if (n == 0)
-    return (1);
-
-return isPalRec(s, 0, n - 1);
+	if (n == 0)
+		return (1);
+	return (isPalRec(s, 0, n - 1));
 }
