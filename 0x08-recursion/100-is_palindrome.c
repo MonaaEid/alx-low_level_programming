68a537c1d669700029c052578b07d7b68f2ab164
MonaaEid <monaa_eid@hotmail.com>
2023-07-05 18:33:49 -0700

    update

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -12,12 +12,17 @@
 int isPalRec(char str[], int s, int e)
 {
 	if (s == e)
+	{
 		return (1);
-
+	}
 	else if (str[s] != str[e])
+	{
 		return (0);
-	else
+	}
+	else if (s < e + 1)
+	{
 		return (isPalRec(str, s + 1, e - 1));
+	}
 }
 
 /**
