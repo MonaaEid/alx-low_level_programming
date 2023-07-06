1b6b51fb9fc004f59edba6e5164ece51457e17c7
MonaaEid <monaa_eid@hotmail.com>
2023-07-05 17:29:52 -0700

    update

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -10,12 +10,10 @@ int is_palindrome(char *s)
 {
 	int len = strlen(s);
 
-	/*
-	 * if (len <= 1)
-		* return (1);
-	*/
-	if (*s != *(s + len - 1))
-		return (0);
-	*(s + len - 1) = '\0';
-	return (is_palindrome(s + 1));
+    if (len <= 1)
+        return (1);
+    if (*s != *(s + len - 1))
+        return (0);
+    *(s + len - 1) = '\0';
+    return (is_palindrome(s + 1));
 }
