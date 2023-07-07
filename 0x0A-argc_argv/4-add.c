76a319cc352629938f28f1c2d70eb598e69d817a
MonaaEid <monaa_eid@hotmail.com>
2023-07-07 14:12:42 -0700

    update

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,25 +13,23 @@ int main(int argc, char *argv[])
 	int sum, i, j;
 
 	sum = 0;
-	for (i = 1; i < argc; i++)
-	{
-                int j;
-                for (j = 0; argv[i][j]; j++)
-                {
-                        if (!isdigit(argv[i][j]))
-                        {
-                                printf("Error\n");
-                                return (1);
-                        }
-                }
-                sum += atoi(argv[i]);
-        }
-
-        printf("%d\n", sum);
 	if (argc == 1)
 	{
 		printf("%d\n", 0);
+		return (0);
 	}
-
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
 	return (0);
 }
