d84c15fd5461f901e46097efad226413f0bc0c00
MonaaEid <monaa_eid@hotmail.com>
2023-07-07 12:41:20 -0700

    update

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -21,30 +21,20 @@ int main(int argc, char *argv[])
 	}
 	cents = atoi(argv[1]);
 	if (cents < 0)
-	{
-		printf("0\n");
-		return (0);
-	}
+	{	printf("0\n");
+		return (0);	}
 	while (cents >= 25)
-	{
-		cents -= 25;
-		coins++;
-	}
+	{	cents -= 25;
+		coins++;	}
 	while (cents >= 10)
-	{
-		cents -= 10;
-		coins++;
-	}
+	{	cents -= 10;
+		coins++;	}
 	while (cents >= 5)
-	{
-		cents -= 5;
-		coins++;
-	}
+	{	cents -= 5;
+		coins++;	}
 	while (cents >= 2)
-	{
-		cents -= 2;
-		coins++;
-	}
+	{	cents -= 2;
+		coins++;	}
 	while (cents >= 1)
 	{
 		cents -= 1;
