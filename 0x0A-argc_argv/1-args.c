8e426e41757f75825b452276fb3680f25c862d18
MonaaEid <monaa_eid@hotmail.com>
2023-07-07 08:46:42 -0700

    update

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * @argc: stores the number of command-line arguments
+ * @argv: array of character pointers listing all the arguments.
+ * Description: a program that prints the number of arguments passed into it.
+ *
+ * Return: Always 0 (Success);
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\", argc);
+	return (0);
+}
