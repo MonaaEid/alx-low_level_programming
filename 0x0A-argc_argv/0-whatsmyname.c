7183a36c0a460323ba5c3bb1c467567839d673e8
MonaaEid <monaa_eid@hotmail.com>
2023-07-07 07:34:06 -0700

    update

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: stores the number of command-line arguments
+ * @argv: array of character pointers listing all the arguments.
+ * Description: a program that prints its name, followed by a new line.
+ *
+ * Return: Always 0 (Success);
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
