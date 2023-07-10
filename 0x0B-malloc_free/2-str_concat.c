8fae4d1ddd06cbe4832dd5325a6261146b509c33
MonaaEid <monaa_eid@hotmail.com>
2023-07-10 10:15:10 -0700

    update

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,29 +10,26 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	    char *concat;
-        int len1 = 0, len2 = 0, i = 0;
+	char *concat;
+	int len1 = 0, len2 = 0, i = 0;
 
-        if (s1 == NULL)
-                s1 = "";
-        if (s2 == NULL)
-                s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[len1])
+		len1++;
+	while (s2[len2])
+		len2++;
+	concat = malloc(sizeof(char) * (len1 + len2 + 1));
 
-        while (s1[len1])
-                len1++;
-        while (s2[len2])
-                len2++;
+	if (concat == NULL)
+		return (NULL);
+	while (*s1)
+		concat[i++] = *s1++;
+	while (*s2)
+		concat[i++] = *s2++;
+	concat[i] = '\0';
 
-        concat = malloc(sizeof(char) * (len1 + len2 + 1));
-
-        if (concat == NULL)
-                return (NULL);
-
-        while (*s1)
-                concat[i++] = *s1++;
-        while (*s2)
-                concat[i++] = *s2++;
-        concat[i] = '\0';
-
-        return (concat);
+	return (concat);
 }
