a268b5f7d6da3ea946db072dce03489b1707bf84
MonaaEid <monaa_eid@hotmail.com>
2023-07-11 11:17:58 -0700

    update

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -13,6 +13,9 @@ char *argstostr(int ac, char **av)
 	int len = 0;
 	char *new_arr;
 
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
@@ -22,13 +25,10 @@ char *argstostr(int ac, char **av)
 		len++;
 	}
 	new_arr = malloc(len * sizeof(char));
-		if (ac == 0 || av == NULL)
-			return (NULL);
-
-		if (new_arr == 0)
-		{
-			return (NULL);
-		}
+	if (new_arr == 0)
+	{
+		return (NULL);
+	}
 	len = 0;
 	for (i = 0; i < ac; i++)
 	{
