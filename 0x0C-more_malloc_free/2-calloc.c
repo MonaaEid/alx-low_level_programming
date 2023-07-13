4f3f9b69d2c4541400b260670429e10596db84d5
MonaaEid <monaa_eid@hotmail.com>
2023-07-13 05:46:47 -0700

    Add First commit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,16 +13,16 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *ptr;
 	unsigned int i;
-	
+
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
 		return (NULL);
-	
+
 	for (i = 0; i < nmemb * size; i++)
 		ptr[i] = 0;
-	
+
 	return ((void *)ptr);
 }
