f9bd5c074b7bff60678a92d6d64c041a89f2c78f
MonaaEid <monaa_eid@hotmail.com>
2023-07-13 16:31:19 -0700

    update

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -92,7 +92,7 @@ int main(int argc, char **argv)
 	if (argc != 3)
 	{
 		printf("Error\n");
-	/*	return (98);*/
+		return (98);
 	}
 
 	num1 = argv[1];
@@ -119,7 +119,7 @@ int main(int argc, char **argv)
 	}
 
 	product = mul(argv[1], argv[2]);
-	printf("%s\n", product);
+	printf("%s", product);
 	free(product);
 
 	return (0);
