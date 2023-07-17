4fad28864d67e46c93ca87c923091303e91dc0df
MonaaEid <monaa_eid@hotmail.com>
2023-07-17 08:46:53 -0700

    update

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,6 +9,8 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	dog *d = {.name = name, .age = age, .owner = owner};
+	if (d == NULL)
+		return;
+	d = {.name = name, .age = age, .owner = owner};
 
 }
