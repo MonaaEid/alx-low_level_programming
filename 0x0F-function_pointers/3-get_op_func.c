f3dcf2a1f0f8fb2fd147869713f42f8d7f74c8e4
MonaaEid <monaa_eid@hotmail.com>
2023-07-19 16:44:02 -0700

    update

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,9 +1,29 @@
- op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i;
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * get_op_func - function returns a pointer to the function that corresponds
+ * to the operator given as a parameter
+ * @s: operators
+ * Return: a pointer
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (op_s[i].op)
+	{
+		if (*(op_s[i].op) == *s)
+			return (op_s[i].f);
+		i++;
+	}
+	return (NULL);
+}
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,1 +1,34 @@
-#include "function_pointers.h"
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: the numbers that will be used to operations
+ * Description: a program that performs simple operations.
+ * Return: 0 or 1
+ */
+int main(int argc, char **argv)
+{
+	int a, b, result;
+
+	if (argc != 4)
+	{
+		printf("Usage: %s <operation> <a> <b>\n", argv[0]);
+		return (1);
+	}
+	char *op = argv[1];
+
+	a = atoi(argv[2]);
+	b = atoi(argv[3]);
+	int (*op_func)(int, int) = get_op_func(op);
+
+	if (op_func == NULL)
+	{
+		printf("Invalid operation: %s\n", op);
+		return (2);
+	}
+	result = op_func(a, b);
+	printf("%d\n", result);
+	return (0);
+}
