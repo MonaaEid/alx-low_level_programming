fb52f60be80ddf7ea1c90d06491a2b486dcad744
MonaaEid <monaa_eid@hotmail.com>
2023-07-19 11:52:19 -0700

    test

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,28 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void print_opcodes(char *start, char *end)
-{
-	char *ptr;
-
-	for (ptr = start; ptr < end; ptr++)
-	{
-		printf("%02x", *ptr & 0xff);
-	}
-  printf("\n");
-}
 int main(int argc, char **argv)
 {
-	if (argc != 2)
-	{
-		fprintf(stderr, "%s\n", argv[0]);
-		return 1;
-	}
+	int i;
+	int bytes;
 
-  int n = atoi(argv[1]);
-  char *start = (char *)main;
-  char *end = start + n;
-
-  print_opcodes(start, end);
-
-  return 0;
+    if (argc != 2)
+    {
+	    printf("Error\n");
+	    return 1;
+    }
+    bytes = atoi(argv[1]);
+    if (bytes < 0) {
+        printf("Error\n");
+        return 2;
+    }
+    unsigned char *ptr = (unsigned char *)main;
+    for (i = 0; i < bytes; i++) {
+        printf("%02x", *(ptr + i));
+    }
+    printf("\n");
+    return 0;
 }
