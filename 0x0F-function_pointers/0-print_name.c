4d99efa730d2894b63a9c2b830a611bffd21d105
MonaaEid <monaa_eid@hotmail.com>
2023-07-19 07:47:14 -0700

    update

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+#include "function_pointers.h"
+/**
+ * print_name - a function that prints a name.
+ * @name: the name.
+ * @f: the function pointer name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - function that executes a function given as
+ * a parameter on each element of an array.
+ * @array: 
+ * @size:
+ * @action:
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+
+}
