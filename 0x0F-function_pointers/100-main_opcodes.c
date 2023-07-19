eb997e86a1600dc027aa384f9e7ca86586ed678b
MonaaEid <monaa_eid@hotmail.com>
2023-07-19 12:12:20 -0700

    test

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,25 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: number of bytes
+ * Return: 0 or 1 or 2
+ */
 int main(int argc, char **argv)
 {
 	int i;
 	int bytes;
 
-    if (argc != 2)
-    {
-	    printf("Error\n");
-	    return 1;
-    }
-    bytes = atoi(argv[1]);
-    if (bytes < 0) {
-        printf("Error\n");
-        return 2;
-    }
-    unsigned char *ptr = (unsigned char *)main;
-    for (i = 0; i < bytes; i++) {
-        printf("%02x", *(ptr + i));
-    }
-    printf("\n");
-    return 0;
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+	unsigned char *ptr = (unsigned char *)main;
+
+	for (i = 0; i < bytes; i++)
+	{
+		printf("%02x", *(ptr + i));
+	}
+	printf("\n");
+	return (0);
 }
