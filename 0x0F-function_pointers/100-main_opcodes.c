5f3e019ae648bcf7185bcf98bafd3cfb52c5b5bf
MonaaEid <monaa_eid@hotmail.com>
2023-07-19 11:45:42 -0700

    test

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 void print_opcodes(char *start, char *end)
 {
 	char *ptr;
@@ -8,4 +9,20 @@ void print_opcodes(char *start, char *end)
 		printf("%02x", *ptr & 0xff);
 	}
   printf("\n");
+}
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		fprintf(stderr, "%s\n", argv[0]);
+		return 1;
+	}
+
+  int n = atoi(argv[1]);
+  char *start = (char *)main;
+  char *end = start + n;
+
+  print_opcodes(start, end);
+
+  return 0;
 }
