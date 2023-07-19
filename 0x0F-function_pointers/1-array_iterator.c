41abd5dc3723cd945ab11947a84e03490de21246
MonaaEid <monaa_eid@hotmail.com>
2023-07-19 10:11:42 -0700

    test

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,6 +10,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
+	if (array == NULL)
+		return;
+	if (action == NULL)
+		return;
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
