20aeb3b55323f2735529065f9eca069a4438d46d
MonaaEid <monaa_eid@hotmail.com>
2023-07-19 20:36:06 -0700

    update

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,9 +7,9 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list args;
-	va_start(args, n);
 	int sum = 0;
 
+	va_start(args, n);
 	for (unsigned int i = 0; i < n; i++)
 	{
 		sum += va_arg(args, int);
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,7 @@
 #ifndef _variadic_functions_h_
 #define _variadic_functions_h_
 #include <stdarg.h>
+#include <stdio.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
