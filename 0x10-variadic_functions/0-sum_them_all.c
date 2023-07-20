a81558d752b83e0813b9d15322dda4c57ceb92ac
MonaaEid <monaa_eid@hotmail.com>
2023-07-20 09:27:07 -0700

    update

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,8 +10,8 @@ int sum_them_all(const unsigned int n, ...)
 	int sum = 0;
 	unsigned int i;
 
-	if (n ==0)
-		return(0);
+	if (n == 0)
+		return (0);
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
