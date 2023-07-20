439ca2ef0a5226ecf8d5f5b11d7cc2b053ae4ef3
MonaaEid <monaa_eid@hotmail.com>
2023-07-20 14:21:14 -0700

    test

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -23,7 +23,7 @@ void print_all(const char * const format, ...)
 				printf("%s%d", sep, va_arg(args, int));
 				break;
 			case 'f':
-				printf("%s%f", sep, va_arg(args, double));
+				printf("%s%fi", sep, va_arg(args, double));
 				break;
 			case 's':
 				{
