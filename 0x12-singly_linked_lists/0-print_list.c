824dbce9f6975ed033f4588e50b9bea9af852032
MonaaEid <monaa_eid@hotmail.com>
2023-07-27 09:14:50 -0700

    update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,17 +9,15 @@
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
-	const list_t *current = h;
 
-	while (current != NULL)
+	while (h != NULL)
 	{
-		if (current->str == NULL)
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("%s\n", current->str);
-
-		current = current->next;
+			printf("[%d] %s\n", h->len, h->str);
 		count++;
+		h = h->next;
 	}
 	return (count);
 }
