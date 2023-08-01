92747088db60a06564ced75ab2f3519cab280b2b
MonaaEid <monaa_eid@hotmail.com>
2023-08-01 13:35:25 -0700

    test

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,4 +1,7 @@
 #include "lists.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
 /**
  * free_listint_safe - frees a listint_t list
  * @h: pointer to pointer to the first node of the list
@@ -7,25 +10,85 @@
 size_t free_listint_safe(listint_t **h)
 {
 	size_t count = 0;
-	listint_t *temp = NULL;
+	listint_t *current, *next, *loop;
 
-	if (!(h && *h))
-		return (count);
-	while (*h)
+	if (h == NULL || *h == NULL)
+		return (0);
+
+	loop = find_iloop(*h);
+	current = *h;
+	while (current != loop)
 	{
+		next = current->next;
+		free(current);
 		count++;
-		if (*h > (*h)->next)
-		{
-			temp = *h;
-			*h = (*h)->next;
-			free(temp);
-		}
-		else
-		{
-			free(*h);
-			*h = NULL;
-		}
+		current = next;
 	}
+	if (loop != NULL)
+	{
+		free_loop(loop);
+		count++;
+	}
+
 	*h = NULL;
 	return (count);
+}
+
+/**
+ * find_iloop - finds the node where the loop starts
+ * @head: pointer to the first node of the list
+ * Return: pointer to the node where the loop starts, or NULL if no loop
+ */
+listint_t *find_iloop(listint_t *head)
+{
+	listint_t *slow, *fast;
+
+	slow = head;
+	fast = head->next;
+	while (fast != NULL && fast->next != NULL)
+	{
+		if (slow == fast)
+			return (find_start(head, slow));
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	return (NULL);
+}
+
+/**
+ * find_start - finds the node where the loop starts
+ * @head: pointer to the first node of the list
+ * @meet: pointer to the node where slow and fast pointers meet
+ * Return: pointer to the node where the loop starts
+ */
+listint_t *find_start(listint_t *head, listint_t *meet)
+{
+	listint_t *start, *end;
+
+	start = head;
+	end = meet;
+	while (start != end)
+	{
+		start = start->next;
+		end = end->next;
+	}
+	return (start);
+}
+
+/**
+ * free_loop - frees a list with a loop
+ * @loop: pointer to the node where the loop starts
+ */
+void free_loop(listint_t *loop)
+{
+	listint_t *current, *next;
+
+	current = loop->next;
+	while (current != loop)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+	free(loop);
 }
