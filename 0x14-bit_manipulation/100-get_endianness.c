d024a74df7ee1415938fa4dc33b82e421955dd4b
MonaaEid <monaa_eid@hotmail.com>
2023-08-03 15:04:25 -0700

    update

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,19 +1,29 @@
 #include "main.h"
 /**
  * get_endianness - a function that checks the endianness.
- * Returns: 0 if big endian, 1 if little endian
+ * Return: 0 if big endian, 1 if little endian
  */
 int get_endianness(void)
 {
-    union {
-        int i;
-        char c[sizeof(int)];
-    } u;
+	/**
+	 * A union that can store an int or a char array
+	 * @union my_union
+	 * @members
+	 *  int i: the int member
+	 *  char c[sizeof(int)]: the char array member
+	 * @var u
+	 * The union variable
+	 */
+	union my_union
+	{
+		int i;
+		char c[sizeof(int)];
+	} u;
 
-    u.i = 1;
+	u.i = 1;
 
-    if (u.c[0] == 1)
-        return (1);
-    else
-        return (0);
+	if (u.c[0] == 1)
+		return (1);
+	else
+		return (0);
 }
