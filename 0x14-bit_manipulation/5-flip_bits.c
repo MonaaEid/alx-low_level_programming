b34a84e01c74080b37c1466ea31094233b65eb36
MonaaEid <monaa_eid@hotmail.com>
2023-08-03 16:50:24 -0700

    update

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -4,28 +4,16 @@
  * @n: bits
  * Return: count of set bits
  */
-int countSetBits(int n)
-{
-	int count = 0;
-
-	while (n > 0)
-	{
-		count += n & 1;
-		n >>= 1;
-	}
-	return (count);
-}
-/**
- * flip_bits -  returns the number of bits you would need to
- * flip to get from one number to another.
- * @n: the first number
- * @m: the second number
- * Return: the number of bits
- */
-
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	int x = n ^ m;
+    unsigned long int x = n ^ m;
+    unsigned int count = 0;
 
-	return (countSetBits(x));
+    while (x > 0)
+    {
+        count += x & 1;
+        x >>= 1;
+    }
+
+    return (count);
 }
