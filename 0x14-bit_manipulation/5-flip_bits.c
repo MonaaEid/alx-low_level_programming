8cfa0df1e58d940f4495291f0e36917ad30db1e3
MonaaEid <monaa_eid@hotmail.com>
2023-08-03 14:07:07 -0700

    update

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -6,3 +6,20 @@
  * @m: 
  * Return: 
  */
+int countSetBits(int n)
+{
+	int count = 0;
+	while (n > 0)
+	{
+		count += n & 1;
+		n >>= 1;
+	}
+	return count;
+}
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int x = n ^ m;
+
+	return countSetBits(x);
+}
