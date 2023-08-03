d5de127bfe6ec3a6face3cc4214ea98836b2dcd6
MonaaEid <monaa_eid@hotmail.com>
2023-08-03 14:24:49 -0700

    update

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -3,4 +3,17 @@
  * get_endianness - a function that checks the endianness.
  * Returns: 0 if big endian, 1 if little endian
  */
+int get_endianness(void)
+{
+    union {
+        int i;
+        char c[sizeof(int)];
+    } u;
 
+    u.i = 1;
+
+    if (u.c[0] == 1)
+        return 1; // Little endian
+    else
+        return 0; // Big endian
+}
--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,25 +1,31 @@
 #include "main.h"
 /**
- * flip_bits -  returns the number of bits you would need to
- * flip to get from one number to another.
- * @n: 
- * @m: 
- * Return: 
+ * countSetBits - function to count set bits in an integer
+ * @n: bits
+ * Return: count of set bits
  */
 int countSetBits(int n)
 {
 	int count = 0;
+
 	while (n > 0)
 	{
 		count += n & 1;
 		n >>= 1;
 	}
-	return count;
+	return (count);
 }
+/**
+ * flip_bits -  returns the number of bits you would need to
+ * flip to get from one number to another.
+ * @n: the first number
+ * @m: the second number
+ * Return: the number of bits
+ */
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	int x = n ^ m;
 
-	return countSetBits(x);
+	return (countSetBits(x));
 }
