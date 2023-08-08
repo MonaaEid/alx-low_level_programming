61eb1022d84fde83abf8adece112c34300eb1e90
MonaaEid <59332150+MonaaEid@users.noreply.github.com>
2023-08-08 03:45:52 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,38 +11,19 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    char *buffer;
-    ssize_t bytes_read, bytes_written; 
+	FILE *fp = fopen(filename, "r");
+	char s;
+	ssize_t printed = 0;
 
-    if (filename == NULL)
-        return (0);
-
- 
-    fd = open(filename, O_RDONLY);
- 
-    if (fd == -1)
-        return (0);
-
-
-    buffer = malloc(sizeof(char) * letters);
-    if (buffer == NULL)
-        return (0);
-
-    bytes_read = read(fd, buffer, letters);
-
-    if (bytes_read == -1)
-        return (0);
-
-    close(fd);
-
-    bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-
-    if (bytes_written == -1 || bytes_written != bytes_read)
-        return (0);
-
-    free(buffer);
-
-    return (bytes_read);
+	if (!fp || !filename)
+		return (0);
+	while (((s = fgetc(fp)) != EOF) && printed < (ssize_t)letters)
+	{
+		write(1, &s, 1);
+		printed++;
+	}
+	if (printed == 0 && letters != 0)
+		return (0);
+	fclose(fp);
+	return (printed);
 }
-
