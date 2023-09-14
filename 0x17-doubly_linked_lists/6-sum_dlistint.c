8cdb35ab260f9839e9371c6ec1eafaf687f2d690
MonaaEid <monaa_eid@hotmail.com>
2023-09-15 02:47:54 +0300

    update

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 #include <stdio.h>
 /**
- * sum_dlistint - returns the sum of all the data (n) of a dlistint_t linked list.
+ * sum_dlistint - returns the sum of all the data of a dlistint_t linked list.
  * @head: head of linked list
  * Return: sum
  */
