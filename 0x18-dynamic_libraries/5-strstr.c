76b48f72202f33407cf6476bd95c3a01cd499fa1
MonaaEid <59332150+MonaaEid@users.noreply.github.com>
2023-09-19 02:58:52 +0300

    Create 5-strstr.c

---
+++ 0x18-dynamic_libraries/5-strstr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strstr - a function that locates a substring.
+ * @haystack: string
+ * @needle: substring
+ * Return: a pointer to the memory area s
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, k;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (k = 0; needle[k] != '\0'; k++)
+		{
+			if (haystack[i + k] != needle[k])
+				break;
+		}
+		if (needle[k] == '\0')
+			return (&haystack[i]);
+	}
+	return (NULL);
+}
