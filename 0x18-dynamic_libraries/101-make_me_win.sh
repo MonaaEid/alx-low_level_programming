7327cc242622b021bcd97979555fc56cce8c9605
MonaaEid <monaa_eid@hotmail.com>
2023-09-19 04:18:51 +0300

    update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/MonaaEid/alx-low_level_programming/blob/master/0x18-dynamic_libraries/gm.so
-export LD_PRELOAD=/tmp/gm.so
+wget -P /tmp https://github.com/MonaaEid/alx-low_level_programming/blob/master/0x18-dynamic_libraries/mona.so
+export LD_PRELOAD=/tmp/mona.so
