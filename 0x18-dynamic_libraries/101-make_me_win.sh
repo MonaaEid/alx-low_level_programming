94e40b72b56a87d1334b71b97ededfcfffaac5ed
MonaaEid <59332150+MonaaEid@users.noreply.github.com>
2023-09-19 04:21:50 +0300

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/MonaaEid/alx-low_level_programming/blob/master/0x18-dynamic_libraries/mona.so
-export LD_PRELOAD=/tmp/mona.so
+wget -P /tmp https://github.com/sidneyriffic/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/putshack.so
+export LD_PRELOAD=/tmp/putshack.so
