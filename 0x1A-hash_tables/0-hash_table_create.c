908aa3a756d25059136ea4d7d0d7907004ba83c1
MonaaEid <monaa_eid@hotmail.com>
2023-10-06 21:36:19 +0300

    update

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -19,7 +19,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (ht == NULL)
 		return (NULL);
 
-	ht->array = calloc(size, sizeof(hash_node_t *));
+	ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (ht->array == NULL)
 	{
 		free(ht);
