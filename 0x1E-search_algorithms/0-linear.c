4d575719a29863e77839439e997c0a03df22f67e
MonaaEid <monaa_eid@hotmail.com>
2024-01-16 15:36:36 +0200

    pack web static

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,12 +9,13 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	int i;
+	size_t i;
 
 	for (i = 0; i < size; i++)
 	{
 		if (array[i] == value)
 			return (i);
-		return (-1);
 	}
+	return (-1);
+
 }
