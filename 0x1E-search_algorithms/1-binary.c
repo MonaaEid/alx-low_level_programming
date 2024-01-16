b1881e4f267db656113fd57d2efd3c19c90b297a
MonaaEid <monaa_eid@hotmail.com>
2024-01-17 00:57:57 +0200

    test

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -9,5 +9,21 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-    
+	size_t m = (size / 2) - 1;
+
+	if (array == NULL && array[m] != value)
+		return (-1);
+	if (array[m] == value)
+		return (m);
+
+	if (value < array[m])
+		return (binary_search(array, m, value));
+	if (value > array[m])
+		return(binary_search(array, m + size, value));
+	else
+		return (-1);
+	/*}*/
 }
+	/* while (size > i) */
+	/* {*/
+		/*printf("Searching in array: %d", array);*/
