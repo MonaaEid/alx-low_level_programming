4423e0a160b224c98abe0d52c0f5d0e6a67f741d
MonaaEid <monaa_eid@hotmail.com>
2024-01-16 15:51:20 +0200

    pack web static

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,6 +13,8 @@ int linear_search(int *array, size_t size, int value)
 
 	for (i = 0; i < size; i++)
 	{
+
+		printf("Value checked array[%lu] = [%d]", i, array[i]);
 		if (array[i] == value)
 			return (i);
 	}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,6 +1,9 @@
 #ifndef SEARCH_ALGOS_H_
 #define SEARCH_ALGOS_H_
 #include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 #endif
